0afe887627acf709926cb6fa66bd011aa71441b1
Toby Allsopp <toby@mi6.gen.nz>
2018-05-23 20:32:02 +1200

    noisy.h: include copy/moove and address

--- noisy.hpp
+++ noisy.hpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
 struct noisy {
-	noisy(int x) : x(x) { std::cout << "noisy()\n"; }
-	~noisy() { std::cout << "~noisy()\n"; }
+	noisy(int x) : x(x) { std::cout << this << ": noisy()\n"; }
+	noisy(noisy const& o) : x(o.x) { std::cout << this << ": noisy(noisy const&)\n"; }
+	noisy(noisy && o) : x(o.x) { std::cout << this << ": noisy(noisy &&)\n"; }
+	~noisy() { std::cout << this << ": ~noisy()\n"; }
 
 	int x;
 
 	friend std::ostream& operator<<(std::ostream& os, noisy const& n) {
-		return os << "NOISY! " << n.x;
+		return os << &n << ": NOISY! " << n.x;
 	}
 };
